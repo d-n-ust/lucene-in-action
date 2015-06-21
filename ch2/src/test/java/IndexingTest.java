e7848d940fc476affec41b6159677d0d3d9a5c8a
Dmitry Ustimov <dmitryustimov@gmail.com>
2015-06-21 09:01:07 +0300

    refactor tests in ch3 module

--- ch2/src/test/java/IndexingTest.java
+++ ch2/src/test/java/IndexingTest.java
@@ -24,8 +24,6 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 // From chapter 2
 public class IndexingTest extends TestCase {
@@ -68,29 +66,6 @@ public class IndexingTest extends TestCase {
         int hitCount = TestUtil.hitCount(searcher, query);     //6
         reader.close();
         return hitCount;
-    }
-
-    protected List<Document> getHits(String fieldName, String searchString)
-            throws IOException {
-        IndexReader reader = DirectoryReader.open(directory);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        Term t = new Term(fieldName, searchString);
-        Query query = new TermQuery(t);
-        TopDocs hits = searcher.search(query, 10);
-
-        List<Document> result = new ArrayList<Document>();
-        for(ScoreDoc scoreDoc : hits.scoreDocs) {
-            result.add(searcher.doc(scoreDoc.doc));
-        }
-
-        reader.close();
-        return result;
-    }
-
-    protected int getNumDocs()
-            throws IOException {
-        IndexReader reader = DirectoryReader.open(directory);
-        return reader.numDocs();
     }
 
     public void testIndexWriter() throws IOException {
@@ -177,75 +152,5 @@ public class IndexingTest extends TestCase {
     #C Verify old document is gone
     #D Verify new document is indexed
   */
-
-    public void testBoostField() throws IOException {
-
-        IndexWriter writer = getWriter();
-        writer.deleteAll();
-        writer.commit();
-        assertEquals(0, getNumDocs());
-
-        Document doc = new Document();
-        doc.add(new StringField("id", "1", Field.Store.YES));
-        doc.add(new TextField("contents", "Moscow", Field.Store.NO));
-        writer.addDocument(doc);
-
-        Document doc2 = new Document();
-        doc2.add(new StringField("id", "2", Field.Store.YES));
-        doc2.add(new TextField("contents", "Moscow", Field.Store.NO));
-        writer.addDocument(doc2);
-
-        Document doc3 = new Document();
-        doc3.add(new StringField("id", "3", Field.Store.YES));
-        doc3.add(new TextField("contents", "Moscow", Field.Store.NO));
-        writer.addDocument(doc3);
-
-        writer.close();
-
-        List<Document> docs = getHits("contents", "Moscow");
-
-        assertEquals(3, getNumDocs());
-        assertEquals(3, docs.size());
-        assertEquals("1", docs.get(0).get("id"));
-        assertEquals("2", docs.get(1).get("id"));
-        assertEquals("3", docs.get(2).get("id"));
-
-        writer = getWriter();
-        writer.deleteAll();
-        writer.commit();
-
-        assertEquals(0, getNumDocs());
-
-        Document doc2_1 = new Document();
-        doc2_1.add(new StringField("id", "1", Field.Store.YES));
-        Field f = new TextField("contents", "Moscow", Field.Store.NO);
-        f.setBoost(1.0f);
-        doc2_1.add(f);
-        writer.addDocument(doc2_1);
-
-        Document doc2_2 = new Document();
-        doc2_2.add(new StringField("id", "2", Field.Store.YES));
-        Field f2 = new TextField("contents", "Moscow", Field.Store.NO);
-        f2.setBoost(100.0f);
-        doc2_2.add(f2);
-        writer.addDocument(doc2_2);
-
-        Document doc2_3 = new Document();
-        doc2_3.add(new StringField("id", "3", Field.Store.YES));
-        Field f3 = new TextField("contents", "Moscow", Field.Store.NO);
-        f3.setBoost(10.0f);
-        doc2_3.add(f3);
-        writer.addDocument(doc2_3);
-
-        writer.close();
-
-        docs = getHits("contents", "Moscow");
-
-        assertEquals(3, getNumDocs());
-        assertEquals(3, docs.size());
-        assertEquals("2", docs.get(0).get("id"));
-        assertEquals("3", docs.get(1).get("id"));
-        assertEquals("1", docs.get(2).get("id"));
-    }
 }
 
